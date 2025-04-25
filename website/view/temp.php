a8129ed66ed44c47ef1cf74ca30f04195117b70e
André <42913113+andre7455@users.noreply.github.com>
2025-04-25 07:11:29 +0200

    Refactor website structure: add header, footer, and temp files; remove old HTML structure

---
+++ website/view/temp.php
@@ -0,0 +1,12 @@
+<?php
+include './view/menu/header.php';
+customHeader('Under Construction', '../assets/css/initialPage.css');
+echo '
+  <div class="construction blinking">Under construction</div>
+  <p>Come back later!</p>
+  <img src="./assets/9XryZQt.jpeg" alt="funny meme" width="300" height="400">
+  <p>Last updated: april 2025</p>
+  ';
+
+include './view/menu/footer.php';
+  ?>
