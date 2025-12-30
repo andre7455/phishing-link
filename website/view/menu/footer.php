5f72fc2aaed0419c38e6d867b311eeee37904c6b
André <42913113+andre7455@users.noreply.github.com>
2025-12-30 19:39:26 +0100

    test CI/CD

--- website/view/menu/footer.php
+++ website/view/menu/footer.php
@@ -4,7 +4,7 @@ function customFooter(){
     echo '
     </body>
 <footer>
-    <p>&copy; ' . date("Y") . '. All rights reserved.</p>
+    <p>&copy; ' . date("Y") . '. All rights reserved. Andre van der Lugt</p>
 </footer>
 
 </html>';
